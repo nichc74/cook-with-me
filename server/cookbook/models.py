b4f5e443ee7bf1e8937ab6b87429b643eab24138
Josh Cheung <JTCheung3@gmail.com>
2021-06-09 23:54:30 -0700

    created models for ingredient, image, and note

--- server/cookbook/models.py
+++ server/cookbook/models.py
@@ -14,8 +14,30 @@ class Recipe(models.Model):
     def __str__(self):
         return  "recipe_title: " + self.title  + "\n" +  "recipe_author: " + self.author + "\n" + "recipe_prep_time: " +  str(self.prep_time) + "\n" + "recipe_cook_time: " +  str(self.cook_time) + "\n" + "recipe_total_time: " +  str(self.total_time) + "\n" + "recipe_meal_type: " +  self.meal_type + "\n" + "recipe_servings: " +  str(self.servings) + "\n"
 
-                
+# class RecipeIngredient(models.Model):
+# # id = models.AutoField(primary_key=True)
+#     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+#     ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+#     quantity = models.FloatField()
 
+#     def __str__(self):
+#         return  "recipe_id: " + self.recipe_id  + "\n" +  "ingredient_id: " + self.ingredient_id + "\n" + "quantity: " +  str(self.quantity) + "\n" 
 
+               
+# class Instruction(models.Model):
+#     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+#     description = models.CharField(max_length=255)
+#     instr_order = models.IntegerField()
+#     image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128)
     
+class Image(models.Model):
+    path = models.CharField(max_length=128)
+
+
+class Note(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
 
