f7c32615004dcaf4509d9e5dc5c18e5e778a18da
Nicholas Cheung <nichc74@gmail.com>
2021-06-15 01:07:27 -0700

    Adding __str__ methods to cookbook.models

--- server/cookbook/models.py
+++ server/cookbook/models.py
@@ -9,16 +9,28 @@ class Recipe(models.Model):
     meal_type = models.CharField(max_length=100)
     servings = models.IntegerField()
 
+    def __str__(self):
+        return "%s: %s" % (self.id, self.title)
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "%s: %s" % (self.id, self.name)
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField()
 
+    def __str__(self):
+        return "%s: %s" % (self.recipe.title, self.ingredient.name)
+
 class Image(models.Model):
     path = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "%s" % (self.path)
         
 class Instruction(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
@@ -26,7 +38,13 @@ class Instruction(models.Model):
     instr_order = models.IntegerField()
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "%s: %s" % (self.id, self.description)
+
 class Note(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "%s: %s" % (self.id, self.description)
 
