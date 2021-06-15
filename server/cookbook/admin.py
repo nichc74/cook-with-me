779a9e4615b7691594d0ebe1eab030c8825c3f97
root <jtcheung97@gmail.com>
2021-06-15 07:47:51 +0000

    registered the models for the cookbook

--- server/cookbook/admin.py
+++ server/cookbook/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from cookbook.models import *
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
+admin.site.register(Image)
+admin.site.register(Instruction)
+admin.site.register(Note)
