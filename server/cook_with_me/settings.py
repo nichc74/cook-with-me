40088af70d83fa1600ce14ceb7a9cbd0e9eafaf3
Josh Cheung <JTCheung97@gmail.com>
2023-07-26 00:24:36 -0700

    updating docker compose file

--- server/cook_with_me/settings.py
+++ server/cook_with_me/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cook_with_me',
-        'USER': 'root',
+        'USER': 'root', 
         'PASSWORD': '',
         'HOST': 'mysql_db',
         'PORT': '3306',
