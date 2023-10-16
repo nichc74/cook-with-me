4c05042f147101e92e349055db5051492559e6c6
Josh Cheung <JTCheung97@gmail.com>
2023-10-16 14:08:20 -0700

    moving client to it's own repo

--- server/settings.py
+++
@@ -1,7 +0,0 @@
-import cloudinary
-          
-cloudinary_config = cloudinary.config( 
-  cloud_name = "dufsumsmb", 
-  api_key = "877846737135694", 
-  api_secret = "er-YAmFTqEniXnRahEAYXM_KmEw" 
-)
--- setup_mysql.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE cook_with_me;
-CREATE USER 'django_admin' IDENTIFIED BY 'N1chola$andj0sh';
-GRANT ALL ON cook_with_me.* TO 'django_admin';
-FLUSH PRIVILEGES;
