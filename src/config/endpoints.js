a6cb0e407410056ec57665e49cb7e169c3f40340
Josh Cheung <JTCheung97@gmail.com>
2023-12-05 14:25:37 -0800

    pushing local db

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,11 +1,12 @@
 module.exports = {
-    CONNECTION_TYPE: "https://",
-    HOSTSITE: "api.foodbaby.qvinyl.app",
-    ORM_PATH: "/data-sync/",
+    CONNECTION_TYPE: "http://",
+    HOSTSITE: "localhost:8000",
+    ORM_PATH: "/api/v1",
 }; 
 
 // module.exports = {
-//     CONNECTION_TYPE: "http://",
-//     HOSTSITE: "localhost:8000",
-//     ORM_PATH: "/api/v1",
+//     CONNECTION_TYPE: "https://",
+//     HOSTSITE: "api.foodbaby.qvinyl.app",
+//     ORM_PATH: "/data-sync/",
 // }; 
+
