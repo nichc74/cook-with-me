56e0f8dc0dc13eb0389426a2abed414931fa59fa
Josh Cheung <JTCheung97@gmail.com>
2023-12-29 18:34:46 -0800

    display recipe search results that are revelant or contains in the title

---
+++ src/components/Basics/SearchComponent/SearchContent.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import SearchList from './SearchList';
+
+const SearchContent = ({searchResults, closeSearch} : any) => {
+    return (
+        <>
+            {
+                searchResults.general_recipes.length > 0 &&
+                <>
+                    <h2 style={{color: "#ababab"}}>Recipes</h2>
+                    <SearchList 
+                        closeSearch={closeSearch}
+                        recipeList={searchResults.general_recipes}/>
+                </>
+            }
+            {
+                searchResults.recipes_with_ingredient.length > 0 &&
+                <>
+                    <h2 style={{color: "#ababab"}}>Revelant Recipes</h2>
+                    <SearchList 
+                        closeSearch={closeSearch}
+                        recipeList={searchResults.recipes_with_ingredient}/>
+                </>
+                 
+            }
+        </>
+    )
+}
+
+export default SearchContent;
