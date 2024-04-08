6c9666b42ce0bc53f07146736873b1119519d2a2
Josh Cheung <JTCheung97@gmail.com>
2024-04-08 11:52:36 -0700

    update dependency for updating recipes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom'; 
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Basics/Navbar/Navbar.tsx';
 import Categories from './components/Collections/Categories';
@@ -10,40 +10,49 @@ import { getRecipes, getCategories, getCuisines } from './apis/AdminAPI/RecipeAP
 import Header from './components/Basics/Header/Header.tsx';
 import AdminPage from './components/Admin/AdminPage.tsx';
 import PageNotFound from './components/Pages/NotFound/PageNotFound.tsx';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { updateRecipes } from './store/actions/recipesActions';
 
 function App() {
     const dispatch = useDispatch();
     const location = useLocation();
-    
+
     const reduxRecipes = useSelector((state) => state.recipesReducer.recipes);
     const [recipes, setRecipes] = useState(reduxRecipes);
     const [categories, setCategories] = useState([]);
     const [cuisines, setCuisines] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const path = '/';
+
 
     useEffect(() => {
         fetchCategories();
         fetchCuisines();
-		switch (location.pathname) {
-			case "/categories":
-				fetchRecipes(location.state.collectionPath, location.state.collectionName);
-				break;
-			case "/cuisines":
-				fetchRecipes(location.state.collectionPath, location.state.collectionName);
-				break;
-			default:
-				fetchRecipes();
-                setRecipes(reduxRecipes);
-				break;
-		}
-       
-	}, [location.pathname])
+        fetchRecipes();
+    }, []);
+
+    useEffect(() => {
+        setLoading(true);
+        switch (location.pathname) {
+            case "/categories":
+            case "/cuisines":
+                fetchRecipes(location.state.collectionPath, location.state.collectionName);
+                break;
+            default:
+                fetchRecipes();
+                break;
+        }
+    }, [location.pathname]);
+
+    useEffect(() => {
+        setRecipes(reduxRecipes);
+        setLoading(false);
+    }, [reduxRecipes]);
 
 
-    const fetchRecipes = async (collection="", collectionName="") => {
+
+    const fetchRecipes = async (collection = "", collectionName = "") => {
         try {
             const fetchedRecipes = await getRecipes(collection, collectionName);
             dispatch(updateRecipes(fetchedRecipes));
@@ -74,33 +83,39 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Header/>
+                <Header />
             </header>
-            <Navbar/>
+            <Navbar />
             <div className="App-main-body">
                 <div className="App-recipeBoxes">
-                    <Routes>
-                        <Route exact path={`${path}admin`} element={<AdminPage/>}/>
-                        <Route exact path={`${path}admin/recipe-form/create`} element={<RecipeForm/> }/>
-                        <Route path={`${path}admin/recipe-form/edit/*`} element={<RecipeForm/>} />
-                        <Route exact path={`${path}/recipes`} element={<Recipes recipes={recipes}/> }/>
-                        <Route path={`${path}recipes/:slug`} element={<Recipe/>}/>
+                    {loading ? (
+                        <div style={{display: "flex", justifyContent: "center"}}>Loading...</div>
+                    ) : (
 
-                        <Route exact path={`${path}/categories`} element={<Categories collections={categories} collectionPath={'categories'} />}/>
-                        {
-                            categories.map((category) => (
-                                <Route key={category.id} path={`${path}categories/:category`} element={<Recipes recipes={recipes}/>}/>
-                            ))
-                        }
-                        <Route exact path={`${path}/cuisines`} element={<Categories collections={cuisines} collectionPath={'cuisines'} />}/>
-                        {
-                            cuisines.map((cuisine) => (
-                                <Route key={cuisine.id} path={`${path}cuisines/:cuisine`} element={<Recipes recipes={recipes}/>}/>
-                            ))
-                        }
-                        <Route exact path="/*" element={<PageNotFound/>}/>
-                        <Route path="/" element={<Navigate to={`${path}recipes`} replace />} />
-                    </Routes>
+                        <Routes>
+                            <Route exact path={`${path}admin`} element={<AdminPage />} />
+                            <Route exact path={`${path}admin/recipe-form/create`} element={<RecipeForm />} />
+                            <Route path={`${path}admin/recipe-form/edit/*`} element={<RecipeForm />} />
+                            <Route exact path={`${path}/recipes`} element={<Recipes recipes={recipes} />} />
+                            <Route path={`${path}recipes/:slug`} element={<Recipe />} />
+
+                            <Route exact path={`${path}/categories`} element={<Categories collections={categories} collectionPath={'categories'} />} />
+                            {
+                                categories.map((category) => (
+                                    <Route key={category.id} path={`${path}categories/:category`} element={<Recipes recipes={recipes} />} />
+                                ))
+                            }
+                            <Route exact path={`${path}/cuisines`} element={<Categories collections={cuisines} collectionPath={'cuisines'} />} />
+                            {
+                                cuisines.map((cuisine) => (
+                                    <Route key={cuisine.id} path={`${path}cuisines/:cuisine`} element={<Recipes recipes={recipes} />} />
+                                ))
+                            }
+                            <Route exact path="/*" element={<PageNotFound />} />
+                            <Route path="/" element={<Navigate to={`${path}recipes`} replace />} />
+                        </Routes>
+                    )
+                    }
                 </div>
             </div>
             {/* <footer className="App-footer">
