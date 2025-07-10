dcc803ea7a4ebb72890af587d1ec6e3a4fc8143e
Josh Cheung <JTCheung97@gmail.com>
2025-07-09 23:18:14 -0700

    Updating endpoint

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,9 +1,9 @@
 // export const CONNECTION_TYPE = "http://";
-// export const HOSTSITE = "localhost:8000";
+// export const HOSTSITE = "localhost:8080";
 // export const ORM_PATH = "/api/v1/"; 
 
 export const CONNECTION_TYPE = "https://";
 export const HOSTSITE = "api.foodbaby.qvinyl.app";
-export const ORM_PATH = "/data-sync/"; 
+export const ORM_PATH = "/api/v1/"; 
 
 // http://localhost:3000/cook-with-me
